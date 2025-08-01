c4cd12ad46444bc278f684163b7143ab638f6934
KINGSAM868 <140064306+KINGSAM868@users.noreply.github.com>
2025-08-01 19:39:38 +0300

    Create answers.sql

---
+++ answers.sql
@@ -0,0 +1,6 @@
+-- This query creates a new database named 'salesDB'.
+CREATE DATABASE salesDB;
+
+-- This query drops (deletes) the database named 'demo'.
+-- The `IF EXISTS` clause prevents an error if the database does not exist.
+DROP DATABASE IF EXISTS demo;
